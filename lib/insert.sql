ed0681df31ba61e72e1e1d5c48d90077c3292fda
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-02-07 18:35:06 -0500

    commit tree d31fd0b1249a34f084a928c872610cb35be40f91

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex) VALUES ('Mr. Chocolate',1,'F');
+INSERT INTO bears(name, age, sex) VALUES ('Rowdy',1, 'F');
+INSERT INTO bears(name, age, sex) VALUES ('Tabitha',3,'M');
+INSERT INTO bears(name, age, sex) VALUES ('Sergeant Brown', 1, 'F');
+INSERT INTO bears(name, age, sex) VALUES ('Melissa', 2, 'M');
+INSERT INTO bears(name, age, sex) VALUES ('Grinch',1, 'M');
+INSERT INTO bears(name, age, sex) VALUES ('Wendy', 2, 'F');
+INSERT INTO bears(name, age, sex) VALUES (NULL, 3, 'F');
